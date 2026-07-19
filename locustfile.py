531c4bb9fc15cd0428dfebf0a62f1e21d92d83bc
Edson-Edwin <edsonedwin01@gmail.com>
2026-07-19 12:06:43 +0530

    changes wiki rest apis to wiki actions apis

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,23 @@
 from locust import HttpUser, task, between
+import uuid
+import random
 
 class WikiAPIUser(HttpUser):
-    # Wait between 1 and 3 seconds between requests per simulated user
-    wait_time = between(1, 3)
+    # ZERO wait time to absolutely hammer the server and maximize CPU/Thread usage
+    wait_time = between(0, 0)
 
     @task(1)
     def get_article_guide(self):
-        # Testing the fast endpoint (Guide generation)
-        self.client.get("/api/article/Python/guide")
+        # We append a random UUID to the title to explicitly BYPASS the @lru_cache.
+        # If we didn't do this, the cache would return the result in 0.0001s 
+        # and you wouldn't see the actual thread limitations.
+        random_title = f"Topic_{uuid.uuid4()}"
+        self.client.get(f"/api/article/{random_title}/guide", name="/api/article/[title]/guide")
 
     @task(3)
     def get_article_references(self):
-        # Testing the heavy multiprocessing endpoint (Paginated references)
-        # We give it a weight of 3 because users are more likely to click 'Load More'
-        self.client.get("/api/article/Python/references?offset=0")
+        # This endpoint spawns 5 threads internally. 
+        # Hammering this will quickly exhaust the ThreadPoolExecutor or Gunicorn workers.
+        random_title = f"Topic_{uuid.uuid4()}"
+        offset = random.choice([0, 5, 10])
+        self.client.get(f"/api/article/{random_title}/references?offset={offset}", name="/api/article/[title]/references")
